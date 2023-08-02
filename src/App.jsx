46e086c585c34e1be65637605ca1740c0595dc40
Reginald Shawn Annan <shawnrannan@gmail.com>
2023-08-02 19:51:13 +0000

    add sign up form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import SignInForm from "./components/SignIn/SignInForm";
+import SignUpForm from "./components/SignUp/SignUpForm";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <div className="vh-100" style={{backgroundImage: 'url("../public/bg.jpg")'}}>
       <Navbar />
       <SignInForm/>
+      <SignUpForm/>
       </div>
     </>
   );
--- src/components/SignUp/SignUpForm.jsx
+++ src/components/SignUp/SignUpForm.jsx
@@ -1,7 +1,28 @@
+import "./signup.css"
+
 export default function SignUpForm() {
   return (
-    <form>
-      <h1>Helo</h1>
+    <div className="form-container">
+    <p className="title">Sign Up</p>
+    <form className="form">
+      <div className="input-group">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" placeholder=""/>
+      </div>
+      <div className="input-group">
+        <label for="username">Email</label>
+        <input type="text" name="username" id="username" placeholder=""/>
+      </div>
+      <div className="input-group">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" placeholder=""/>
+        
+      </div>
+      <button className="sign">Sign Up</button>
     </form>
+    <p className="signup">Already have an account?
+      <a rel="noopener noreferrer" href="#" class="">Sign in</a>
+    </p>
+  </div>
   );
 }
